45860e5119a609e6b60350df74755d2bc7bee332
Vidit Pawar <viditpawar0@gmail.com>
2025-09-28 18:36:21 +0530

    modified routing

--- src/main/java/com/studybuddy/topic_trainer/controllers/TopicController.java
+++ src/main/java/com/studybuddy/topic_trainer/controllers/TopicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("topic")
+@RequestMapping("topics")
 public class TopicController {
     private final TopicService topicService;
 
