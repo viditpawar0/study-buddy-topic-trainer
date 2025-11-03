896bee177276aef613671051e21b69b10a17e04c
Vidit Pawar <viditpawar0@gmail.com>
2025-11-04 03:23:12 +0530

    updated scratch controller

--- src/main/java/com/studybuddy/topic_trainer/ScratchController.java
+++ src/main/java/com/studybuddy/topic_trainer/ScratchController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("scratch2")
+@RequestMapping("scratch1")
 public class ScratchController {
     @GetMapping
     public String scratch() {
